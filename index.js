bfe4df0313ae758b925322f3919fe5a1db761898
user\user1 <a006000>
2018-01-24 20:40:28 +0800

    LineBot

--- index.js
+++ index.js
@@ -11,13 +11,15 @@ bot.on('message', function(event) {
 	if (event.message.type = 'text') {
 		var msg = event.message.text;
 		
-		event.reply(msg.indexOf('1')).then(function(data) {
-		//success
-		console.log(msg);
-		}).catch (function(error) {
-			//error
-			console.log('error');
-		});	
+		if(msg.indexOf('1') != -1) {
+			event.reply("有" + msg.indexOf('1') + "個1").then(function(data) {
+			//success
+			console.log(msg);
+			}).catch (function(error) {
+				//error
+				console.log('error');
+			});	
+		}
 		
 			
 	}
