2d4c8f254ca377f0dd95865fa91222fc37975b2d
user\user1 <a006000>
2018-01-24 23:41:55 +0800

    LineBot

--- index.js
+++ index.js
@@ -36,14 +36,14 @@ function _bot() {
 					}
 				});        
 			}
-			if (msg.indexOf('UVI') != -1) {
+			if ((msg.indexOf('UVI') != -1) || (msg.indexOf('紫外線'))) {
 				uviInfo.forEach(function(e, i) {
 					if ((msg.indexOf(e[0]) != -1) && (msg.indexOf(e[1]) != -1) ) {
 						replyMsg = e[0] + e[1] + "的UVI為" + e[2];
 					}else if ((msg.indexOf(e[0]) != -1) && (msg.indexOf(e[1]) == -1) ) {
 						replyMsg = "請輸入區域名稱";
 					}else if ((msg.indexOf(e[0]) == -1) && (msg.indexOf(e[1]) != -1 )) {
-						replyMsg = e[0] + e[1] + "的UVI為" + e[2];
+						replyMsg = e[1] + "的UVI為" + e[2];;
 					}
 				});        
 			}
