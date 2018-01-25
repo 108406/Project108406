bf225ad6d4e75ccfce07d261f89359a493b5ced5
user\user1 <a006000>
2018-01-25 23:29:36 +0800

    LineBot

--- index.js
+++ index.js
@@ -38,53 +38,104 @@ function _bot() {
 		var userC = 0;
 		var isGroup = false;
 		var isUser = false;
+		
+		console.log ('1');
+		
 		if (event.source.groupId != undefined) {
+			
+		console.log ('2');
+		
 			isGroup = true;
 			var pushIn = false;
 			if (groupID.length != 0) {
+				
+		console.log ('3');
+		
 				for (var i = 0; i <= groupID.length - 1 ; i++) {
+					
+		console.log ('4');
+		
 					if (event.source.groupId == groupID[i]) {
-						if (groupIsAnswer[i] == undefined) {						
+						
+		console.log ('5');
+		
+						if (groupIsAnswer[i] == undefined) {		
+
+		console.log ('6');
+								
 							groupIsAnswer[i] = true;
 						}
 						groupC = i;
 					}else {
+						
+		console.log ('7');
+		
 						pushIn = true;
 					}
 				}
 				if (pushIn) {
+					
+		console.log ('8');
+		
 					groupID.push(event.source.groupId);
 					groupC = groupID.length-1;
 					groupIsAnswer[groupC] = true;
 					pushIn = false;
 				}
 			} else {
+				
+		console.log ('9');
+		
 				groupID.push(event.source.groupId);
 				groupC = groupID.length-1;
 				groupIsAnswer[groupC] = true;
 				pushIn = false;
 			}
 		} else {
+			
+		console.log ('10');
+		
 			isUser = true;
 			var pushIn = false;
 			if (userID.length != 0) {
+				
+		console.log ('11');
+		
 				for (var i = 0; i <= userID.length - 1 ; i++) {
+					
+		console.log ('12');
+		
 					if (event.source.userId == userID[i]) {
-						if (userIsAnswer[i] == undefined) {						
+						
+		console.log ('13');
+		
+						if (userIsAnswer[i] == undefined) {		
+
+		console.log ('14');
+								
 							userIsAnswer[i] = true;
 						}
 						userC = i;
 					}else {
+						
+		console.log ('15');
+		
 						pushIn = true;
 					}
 				}
 				if (pushIn) {
+					
+		console.log ('16');
+		
 					userID.push(event.source.userId);
 					userC = userID.length-1;
 					userIsAnswer[userC] = true;
 					pushIn = false;
 				}
 			} else { 
+			
+		console.log ('17');
+		
 				userID.push(event.source.userId);
 				userC = userID.length-1;
 				userIsAnswer[userC] = true;
@@ -92,6 +143,7 @@ function _bot() {
 			}
 		}
 	
+		console.log (groupC + ', ' + userC + ', ' + isGroup + ', ' + isUser);
 	
 		if (event.message.type == 'text') {
 			var msg = event.message.text;
