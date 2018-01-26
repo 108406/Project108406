adc0e353ff6c462fb8206a150e3ce520b3c0c11e
user\user1 <a006000>
2018-01-26 21:03:02 +0800

    LineBot

--- index.js
+++ index.js
@@ -223,10 +223,10 @@ function _bot() {
 								'//q後面的片段為：' + msg.slice((msg.indexOf('//q') + 3), msg.indexOf('//a')) + '\n' +
 								'//a後面的片段為：' + msg.slice((msg.indexOf('//a') + 3), msg.length);
 						}
-						replyMsg = 
+						/*replyMsg = 
 							'//q的indexOf值為' + msg.indexOf('//q') + '\n' + 
 							'//a的indexOf值為' + msg.indexOf('//a') + '\n' +
-							'msg的長度為' + (msg.length); 
+							'msg的長度為' + (msg.length); */
 						/*replyMsg = 
 							'無法辨別「' + msg + '」的意義\n' + 
 							'如果你願意幫助休比回答問題\n' + 
