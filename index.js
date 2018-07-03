69fb89289e6dcd4aaff72a3f17761ea19bae3ff8
HasegawaKobato <a006000@gmail.com>
2018-07-03 23:32:11 +0800

    AI Test

--- index.js
+++ index.js
@@ -67,7 +67,7 @@ function _bot() {
       var containCount = 0;
       for (var i = 0; i <= msg.length - 1; i++) {
         for (var a = 0; a <= aiDB[0].length - 1; a++) {
-          if (msg.indexOf(aiDB[0][a], i) != -1) {
+          if (msg.substr(i,aiDB[0][a].length) == aiDB[0][a]) {
             containCount++;
             console.log ('i: ' + i + ', a: ' + a);
           }
