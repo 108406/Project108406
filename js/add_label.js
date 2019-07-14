ae269e851e319198d85233ec20ad9191582cec25
108406 <ntub108406@gmail.com>
2019-07-15 00:39:55 +0800

    標籤新增1/4

---
+++ js/add_label.js
@@ -0,0 +1,150 @@
+$(document).ready(function () {
+    isTrue = true;
+
+    $("[name='green']").on('click', function () {
+        getcolor ='label-color-green-a';
+        $("[name='green-check']").css({ 'display': 'block' });
+        $("[name='yellow-check']").css({ 'display': 'none' });
+        $("[name='orange-check']").css({ 'display': 'none' });
+        $("[name='red-check']").css({ 'display': 'none' });
+        $("[name='purple-check']").css({ 'display': 'none' });
+        $("[name='blue-check']").css({ 'display': 'none' });
+        $("[name='sky-check']").css({ 'display': 'none' });
+        $("[name='lime-check']").css({ 'display': 'none' });
+        $("[name='pink-check']").css({ 'display': 'none' });
+        $("[name='black-check']").css({ 'display': 'none' });
+    })
+
+    $("[name='yellow']").on('click', function () {
+        getcolor ='label-color-yellow-a';
+        $("[name='green-check']").css({ 'display': 'none' });
+        $("[name='yellow-check']").css({ 'display': 'block' });
+        $("[name='orange-check']").css({ 'display': 'none' });
+        $("[name='red-check']").css({ 'display': 'none' });
+        $("[name='purple-check']").css({ 'display': 'none' });
+        $("[name='blue-check']").css({ 'display': 'none' });
+        $("[name='sky-check']").css({ 'display': 'none' });
+        $("[name='lime-check']").css({ 'display': 'none' });
+        $("[name='pink-check']").css({ 'display': 'none' });
+        $("[name='black-check']").css({ 'display': 'none' });
+    })
+
+    $("[name='orange']").on('click', function () {
+        getcolor ='label-color-orange-a';
+        $("[name='green-check']").css({ 'display': 'none' });
+        $("[name='yellow-check']").css({ 'display': 'none' });
+        $("[name='orange-check']").css({ 'display': 'block' });
+        $("[name='red-check']").css({ 'display': 'none' });
+        $("[name='purple-check']").css({ 'display': 'none' });
+        $("[name='blue-check']").css({ 'display': 'none' });
+        $("[name='sky-check']").css({ 'display': 'none' });
+        $("[name='lime-check']").css({ 'display': 'none' });
+        $("[name='pink-check']").css({ 'display': 'none' });
+        $("[name='black-check']").css({ 'display': 'none' });
+    })
+    
+    $("[name='red']").on('click', function () {
+        getcolor ='label-color-red-a';
+        $("[name='green-check']").css({ 'display': 'none' });
+        $("[name='yellow-check']").css({ 'display': 'none' });
+        $("[name='orange-check']").css({ 'display': 'none' });
+        $("[name='red-check']").css({ 'display': 'block' });
+        $("[name='purple-check']").css({ 'display': 'none' });
+        $("[name='blue-check']").css({ 'display': 'none' });
+        $("[name='sky-check']").css({ 'display': 'none' });
+        $("[name='lime-check']").css({ 'display': 'none' });
+        $("[name='pink-check']").css({ 'display': 'none' });
+        $("[name='black-check']").css({ 'display': 'none' });
+    })
+
+    $("[name='purple']").on('click', function () {
+        getcolor ='label-color-purple-a';
+        $("[name='green-check']").css({ 'display': 'none' });
+        $("[name='yellow-check']").css({ 'display': 'none' });
+        $("[name='orange-check']").css({ 'display': 'none' });
+        $("[name='red-check']").css({ 'display': 'none' });
+        $("[name='purple-check']").css({ 'display': 'block' });
+        $("[name='blue-check']").css({ 'display': 'none' });
+        $("[name='sky-check']").css({ 'display': 'none' });
+        $("[name='lime-check']").css({ 'display': 'none' });
+        $("[name='pink-check']").css({ 'display': 'none' });
+        $("[name='black-check']").css({ 'display': 'none' });
+    })
+
+    $("[name='blue']").on('click', function () {
+        getcolor ='label-color-blue-a';
+        $("[name='green-check']").css({ 'display': 'none' });
+        $("[name='yellow-check']").css({ 'display': 'none' });
+        $("[name='orange-check']").css({ 'display': 'none' });
+        $("[name='red-check']").css({ 'display': 'none' });
+        $("[name='purple-check']").css({ 'display': 'none' });
+        $("[name='blue-check']").css({ 'display': 'block' });
+        $("[name='sky-check']").css({ 'display': 'none' });
+        $("[name='lime-check']").css({ 'display': 'none' });
+        $("[name='pink-check']").css({ 'display': 'none' });
+        $("[name='black-check']").css({ 'display': 'none' });
+    })
+
+    $("[name='sky']").on('click', function () {
+        getcolor ='label-color-sky-a';
+        $("[name='green-check']").css({ 'display': 'none' });
+        $("[name='yellow-check']").css({ 'display': 'none' });
+        $("[name='orange-check']").css({ 'display': 'none' });
+        $("[name='red-check']").css({ 'display': 'none' });
+        $("[name='purple-check']").css({ 'display': 'none' });
+        $("[name='blue-check']").css({ 'display': 'none' });
+        $("[name='sky-check']").css({ 'display': 'block' });
+        $("[name='lime-check']").css({ 'display': 'none' });
+        $("[name='pink-check']").css({ 'display': 'none' });
+        $("[name='black-check']").css({ 'display': 'none' });
+    })
+
+    $("[name='lime']").on('click', function () {
+        getcolor ='label-color-lime-a';
+        $("[name='green-check']").css({ 'display': 'none' });
+        $("[name='yellow-check']").css({ 'display': 'none' });
+        $("[name='orange-check']").css({ 'display': 'none' });
+        $("[name='red-check']").css({ 'display': 'none' });
+        $("[name='purple-check']").css({ 'display': 'none' });
+        $("[name='blue-check']").css({ 'display': 'none' });
+        $("[name='sky-check']").css({ 'display': 'none' });
+        $("[name='lime-check']").css({ 'display': 'block' });
+        $("[name='pink-check']").css({ 'display': 'none' });
+        $("[name='black-check']").css({ 'display': 'none' });
+    })
+
+    $("[name='pink']").on('click', function () {
+        getcolor ='label-color-pink-a';
+        $("[name='green-check']").css({ 'display': 'none' });
+        $("[name='yellow-check']").css({ 'display': 'none' });
+        $("[name='orange-check']").css({ 'display': 'none' });
+        $("[name='red-check']").css({ 'display': 'none' });
+        $("[name='purple-check']").css({ 'display': 'none' });
+        $("[name='blue-check']").css({ 'display': 'none' });
+        $("[name='sky-check']").css({ 'display': 'none' });
+        $("[name='lime-check']").css({ 'display': 'none' });
+        $("[name='pink-check']").css({ 'display': 'block' });
+        $("[name='black-check']").css({ 'display': 'none' });
+    })
+
+    $("[name='black']").on('click', function () {
+        getcolor ='label-color-black-a';
+        $("[name='green-check']").css({ 'display': 'none' });
+        $("[name='yellow-check']").css({ 'display': 'none' });
+        $("[name='orange-check']").css({ 'display': 'none' });
+        $("[name='red-check']").css({ 'display': 'none' });
+        $("[name='purple-check']").css({ 'display': 'none' });
+        $("[name='blue-check']").css({ 'display': 'none' });
+        $("[name='sky-check']").css({ 'display': 'none' });
+        $("[name='lime-check']").css({ 'display': 'none' });
+        $("[name='pink-check']").css({ 'display': 'none' });
+        $("[name='black-check']").css({ 'display': 'block' });
+    })
+
+    $('#add-label-btn').on('click', function () {
+        input_value = $('#get-add-input').val();
+        $("[name='label-color-all-right']").append(function(){
+            return "<div class='label-total'>"+"<a href='#' class="+getcolor+">"+input_value+"</a>"+"<button class='label-color-edit-btn'><i class='fas fa-pen'></i ></button ></div>";
+          });
+    })
+});
--- js/dropdown-show.js
+++ js/dropdown-show.js
@@ -1,13 +1,90 @@
 $(document).ready(function(){
     var isTrue=true;
+    var isTrue2=true;
+    var isTrue3=true;
+    var isTrue4=true;
     $("#edit-label-dropdown").on('click',function(e){
         if(isTrue){
-            $('.dropdown-content').css({'display':'block'});
+            $('div.dropdown-content').css({'display':'block'});
             isTrue=false;
         }
         else{
-            $('.dropdown-content').css({'display':'none'});
+            $('div.dropdown-content').css({'display':'none'});
             isTrue=true;
         }
+        $(document).on("click", function () {
+            $('div.dropdown-content').css({'display':'none'});
+            isTrue=true;
+        })
+        e.stopPropagation();
     })
+    $("#label-edit-off").on('click',function(e){
+            $('div.dropdown-content').css({'display':'none'});
+            isTrue=true;
+    })
+
+
+    $("#add-new-label").on('click',function(e){
+        if(isTrue2){
+            $('.dropdown-content-add-label').css({'display':'block'});
+            isTrue2=false;
+        }
+        else{
+            $('.dropdown-content-add-label').css({'display':'none'});
+            isTrue2=true;
+        }
+        $(document).on("click", function () {
+            $('.dropdown-content-add-label').css({'display':'none'});
+            isTrue2=true;
+        })
+        e.stopPropagation();
+    })
+    $("#add-label-off").on('click',function(e){
+            $('.dropdown-content-add-label').css({'display':'none'});
+            isTrue2=true;
+    })
+
+
+    $("#edit-right-dropdown").on('click',function(e){
+        if(isTrue3){
+            $('.dropdown-content-right').css({'display':'block'});
+            isTrue3=false;
+        }
+        else{
+            $('.dropdown-content-right').css({'display':'none'});
+            isTrue3=true;
+        }
+        $(document).on("click", function () {
+            $('.dropdown-content-right').css({'display':'none'});
+            isTrue3=true;
+        })
+        e.stopPropagation();
+    })
+    $("#label-edit-right-off").on('click',function(e){
+            $('.dropdown-content-right').css({'display':'none'});
+            isTrue3=true;
+    })
+
+
+    $("#add-new-right-label").on('click',function(e){
+        if(isTrue4){
+            $('.dropdown-content-add-right-label').css({'display':'block'});
+            isTrue4=false;
+        }
+        else{
+            $('.dropdown-content-add-right-label').css({'display':'none'});
+            isTrue4=true;
+        }
+        $(document).on("click", function () {
+            $('.dropdown-content-add-right-label').css({'display':'none'});
+            isTrue4=true;
+        })
+        e.stopPropagation();
+    })
+    $("#add-label-right-off").on('click',function(e){
+            $('.dropdown-content-add-right-label').css({'display':'none'});
+            isTrue4=true;
+    })
+
+
 });
