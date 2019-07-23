7f8898228fd0eac2486acf79eb03db1164f7ed6a
Unknown <raralei8860@gmail.com>
2019-07-23 21:38:24 +0800

    Update mywork display complete

--- web/routes/mywork.js
+++ web/routes/mywork.js
@@ -6,21 +6,19 @@ const view = require('./utility/view');
 
 //接收GET請求
 router.get('/', function (req, res, next) {
+    var tampTime = Date.now();
     view.workWithUser('A001').then(data => {
         if (data == null) {
             res.render('error');  //導向錯誤頁面
         } else if (data.rows.length > 0) {
             var result = SetProjectResult(data);
 
-            console.log(result);
-            console.log(result[0].options);
-
-            // res.render('mywork.ejs', { "items": result });  //將資料傳給顯示頁面
+            res.render('mywork.ejs', { "items": result });  //將資料傳給顯示頁面
+            console.log(Date.now() - tampTime);
         } else {
             res.render('notFound');  //導向找不到頁面
         }
     })
-
 });
 
 function SetProjectResult(data) {
@@ -31,14 +29,15 @@ function SetProjectResult(data) {
     for (var i = 0; i < data.rowCount; i++) {
         var project = [];
 
+        //確認project_id不重複
         if (!projectId.includes(data.rows[i].project_id)) {
             projectId.push(data.rows[i].project_id);
             project.push({
                 "project_id": data.rows[i].project_id,
                 "project_name": data.rows[i].project_name
             });
-            var listwork = SetListResult(data, project);
-            var options = SetOptions(project);
+            var listwork = SetListResult(data, data.rows[i].project_id);  //整理列表
+            var options = SetOptions(data, data.rows[i].project_id);  //整理列表選項
             result.push({ project, listwork, options });
         }
     }
@@ -46,51 +45,42 @@ function SetProjectResult(data) {
     return result;
 }
 
-function SetOptions(project) {
-    var project;
+function SetOptions(data, project_id) {
+    var data;
+    var project_id;
     var options = [];
+    var id = [];
 
-    for (var i = 0; i < project.length; i++) {
-        view.listWithProject(project[i].project_id).then(data => {
-            if (data == null) {
-                console.log(error);
-            } else if (data.rowCount > 0) {
-                var Name = [];
-
-                for (var l = 0; l < data.rowCount; l++) {
-                    if (!Name.includes(data.rows[l].list_name)) {
-                        options.push(data.rows[l].list_name);
-                    }
-                }
-
-                console.log("222" + options);
-            } else {
-                console.log('notFound');
+    for (var i = 0; i < data.rowCount; i++) {           //確認是同一個project
+        if (data.rows[i].project_id == project_id) {    //確認list_id不重複
+            if (!id.includes(data.rows[i].list_id)) {
+                id.push(data.rows[i].list_id);
+                options.push(data.rows[i].list_name);
             }
-        })
+        }
     }
 
     return options;
 }
 
-function SetListResult(data, project) {
+function SetListResult(data, project_id) {
     var data;
-    var project;
+    var project_id;
     var result = [];
     var listId = [];
 
-    for (var i = 0; i < project.length; i++) {
-        for (var j = 0; j < data.rowCount; j++) {
-            var list = [];
+    for (var j = 0; j < data.rowCount; j++) {
+        var list = [];
 
-            if (data.rows[j].project_id == project[i].project_id) {
-                if (!listId.includes(data.rows[j].list_id)) {
+        if (data.rows[j].project_id == project_id) {        //確認是同個project
+            if (!listId.includes(data.rows[j].list_id)) {   //確認list_id不重複
+                if (data.rows[j].work_id != null) {         //確認列表裡有工作
                     listId.push(data.rows[j].list_id);
                     list.push({
                         "list_id": data.rows[j].list_id,
                         "list_name": data.rows[j].list_name
                     });
-                    work = SetWorkResult(data, list);
+                    work = SetWorkResult(data, data.rows[j].list_id);
                     result.push({ list, work });
                 }
             }
@@ -101,27 +91,25 @@ function SetListResult(data, project) {
 
 }
 
-function SetWorkResult(data, list) {
+function SetWorkResult(data, list_id) {
     var data;
-    var list;
+    var list_id;
     var result = [];
     var workId = [];
 
-    for (var i = 0; i < list.length; i++) {
-        for (j = 0; j < data.rowCount; j++) {
-            if (data.rows[j].list_id == list[i].list_id) {
-                workId.push(data.rows[j].work_id);
-                result.push({
-                    "work_id": data.rows[j].work_id,
-                    "work_title": data.rows[j].work_title,
-                    "work_content": data.rows[j].work_content,
-                    "deadline": data.rows[j].deadline,
-                    "tag": data.rows[j].tag,
-                    "file": data.rows[j].file,
-                    "first_principal": data.rows[j].first_principal,
-                    "second_principal": data.rows[j].second_principal
-                });
-            }
+    for (var j = 0; j < data.rowCount; j++) {
+        if (data.rows[j].list_id == list_id) { //確認同一個list
+            workId.push(data.rows[j].work_id);
+            result.push({
+                "work_id": data.rows[j].work_id,
+                "work_title": data.rows[j].work_title,
+                "work_content": data.rows[j].work_content,
+                "deadline": data.rows[j].deadline,
+                "tag": data.rows[j].tag,
+                "file": data.rows[j].file,
+                "first_principal": data.rows[j].first_principal,
+                "second_principal": data.rows[j].second_principal
+            });
         }
     }
 
