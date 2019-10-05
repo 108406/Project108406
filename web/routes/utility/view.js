2b25560d63c32153b267b2e555320efe79c296d4
108406 <ntub108406@gmail.com>
2019-10-05 17:21:00 +0800

    add myWorkAllData Function

--- web/routes/utility/view.js
+++ web/routes/utility/view.js
@@ -10,9 +10,9 @@ var projectWithUser = async function (user_id) {
     //讀取資料庫
     await query("select project_id, project_name, project_password, project_startdate, project_enddate from projectwithuser_view where user_id = '" + user_id + "'")
         .then((data) => {
-            result = data;  //成功
+            result = data; //成功
         }, (error) => {
-            result = false;  //執行錯誤
+            result = false; //執行錯誤
             console.log(error)
         });
 
@@ -27,10 +27,10 @@ var projectAllData = async function (project_id) {
     //讀取資料庫
     await query("select * from project_view where project_id = '" + project_id + "'")
         .then((data) => {
-            result = data.rows;  //成功
+            result = data.rows; //成功
             // console.log(data.rows);
         }, (error) => {
-            result = 'failed';  //執行錯誤
+            result = 'failed'; //執行錯誤
             console.log(error)
         });
 
@@ -58,5 +58,27 @@ var workWithUser = async function (user_id) {
     return result;
 }
 
+var myWorkAllData = async function () {
+    //存放結果
+    var result = [];
+    //讀取資料庫
+    await query("select * from mywork_view")
+        .then((data) => {
+            result = data.rows; //成功
+            // console.log(data.rows);
+        }, (error) => {
+            result = 'failed'; //執行錯誤
+            console.log(error)
+        });
+
+    //回傳執行結果
+    return result;
+}
+
 //匯入
-module.exports = { projectWithUser, projectAllData, workWithUser }
+module.exports = {
+    projectWithUser,
+    projectAllData,
+    workWithUser,
+    myWorkAllData
+}
