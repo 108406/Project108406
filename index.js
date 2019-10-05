a0bc9161465bafa239fa7eb24832755cd4465302
108406 <ntub108406@gmail.com>
2019-10-05 21:11:50 +0800

    update

--- index.js
+++ index.js
@@ -141,6 +141,7 @@ let push = setInterval(function () {
 				}
 				console.log(pushMessage)
 			}
+			console.log(pushMessage)
 			if (pushMessage) {
 				pushProjectMessage = 'Hi! ' + allWorkData[allDataIndex].member_name + '\n' +
 					'您的專案【' + allWorkData[allDataIndex].project_name + '】將在\n' +
