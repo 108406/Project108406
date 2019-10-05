c2fa581526284e58b1eb4744be8a8cb324700f10
108406 <ntub108406@gmail.com>
2019-10-05 21:05:34 +0800

    update

--- index.js
+++ index.js
@@ -131,15 +131,15 @@ let push = setInterval(function () {
 				hintDay = project_enddate[2];
 			}
 			if (hintHour == nowDateArray[3] && hintDay == nowDateArray[2]) {
-				// for (let a = 0; a < 6; a++) {
-				// 	if (a != 3 && a != 2 && nowDateArray[a] != project_enddate[a]) {
-				// 		pushMessage = false;
-				// 	}
-				// }
+				for (let a = 0; a < 6; a++) {
+					if (a != 3 && a != 2 && nowDateArray[a] != project_enddate[a]) {
+						pushMessage = false;
+					}
+				}
 			}
-			console.log(nowDateArray)
-			console.log(project_enddate)
 			if (pushMessage) {
+				console.log(nowDateArray)
+				console.log(project_enddate)
 				pushProjectMessage = 'Hi! ' + allWorkData[allDataIndex].member_name + '\n' +
 					'您的專案【' + allWorkData[allDataIndex].project_name + '】將在\n' +
 					project_enddate[0] + '/' + project_enddate[1] + '/' + project_enddate[2] + ' ' +
