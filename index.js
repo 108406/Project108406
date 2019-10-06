d6f336a2065098b39faa660b0710acc00f36edce
108406 <ntub108406@gmail.com>
2019-10-06 12:44:01 +0800

    update

--- index.js
+++ index.js
@@ -174,7 +174,7 @@ let push = setInterval(function () {
 					workPushMessage_7d = false;
 				}
 			}
- 
+			
 			// 在三天以前提醒專案到期
 			let workPushTime_1m = myFunction.BeforeDate(deadline, [0, 0, 3, 0, 0, 0]);
 			let workPushMessage_1m = true;
