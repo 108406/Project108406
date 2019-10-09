97cfa4a2cc952f316df5608e2d31edcf58c6d065
108406 <ntub108406@gmail.com>
2019-10-09 23:45:27 +0800

    update

--- index.js
+++ index.js
@@ -452,13 +452,12 @@ function _bot() {
 								"text": "除了「幫助」以外。所有的命令都是以hash（#）開頭\n所有可執行的命令如下表所示",
 								"align": "center",
 								"size": "xs",
-								"offsetTop": "10px",
-								"paddingBottom": "10px"
+								"offsetTop": "10px"
 							}, {
 								"type": "box",
 								"layout": "horizontal",
 								"cornerRadius": "md",
-								"paddingBottom": "10px",
+								"offsetBottom": "10px",
 								"backgroundColor": "#CCCCFFFF",
 								"contents": [{
 										"type": "text",
